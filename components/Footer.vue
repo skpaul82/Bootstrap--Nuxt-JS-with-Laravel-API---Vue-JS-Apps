0ca932da8f644a42caa27d50fcad0d907c5c35d2
skpaul82 <skpaul82@gmail.com>
2021-03-28 00:11:22 +0600

    Fixed footer at bottom and center aligned copyright text.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <footer class="footer mt-auto py-3 bg-dark text-white">
-    <div class="container">
+  <footer class="footer mt-auto py-3 bg-dark text-white fixed-bottom">
+    <div class="container text-center">
 
-      <copyright class="row text-center text-muted">
-        Build with <i class="fas fa-heart"></i> by SanjoyPaul.com
+      <!-- <div class="col-12"></div> -->
+
+      <copyright class="col-12 text-muted">
+        Build with <i class="fas fa-heart"></i> by <a target="_blank" href="//SanjoyPaul.com">SanjoyPaul.com</a>
       </copyright>
     </div>
   </footer>
